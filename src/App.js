3c7bcdb2268abbc0dd2118dcb1ab866c06a3dde4
Fellype Oliveira <113564265+1fellype@users.noreply.github.com>
2024-06-21 15:22:57 -0300

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [paid, setPaid] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  let paypalRef = useRef();
+
+  const product = {
+    price: 19.99,
+    description: "Plano: Menu da Casa",
+  }
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    const id = "AQdNQQ111WhAEa4UAEgfeH9mf3hTMTAPlY59gPyBhjDWAd6pRpuDXrl0LV8rSUh9IhpQOFXN4Qcafz31"
+    script.src = `https://www.paypal.com/sdk/js?currency=BRL&client-id=${id}`
+
+    script.addEventListener("load", () => setLoaded(true));
+
+    document.body.appendChild(script);
+
+    if (loaded) {
+      function loadButtonsAndLogicAboutPayment() {
+        setTimeout(() => {
+          window.paypal
+          .Buttons({
+            createOrder: (data, actions) => {
+              return actions.order.create({
+                purchase_units: [{
+                  description: product.description,
+                  amount: {
+                    currency_code: "BRL",
+                    value: product.price
+                  }
+                }
+              ]
+              });
+            },
+            onApprove: async (_,actions) => {
+              const order = await actions.order.capture();
+
+              setPaid(true);
+
+              console.log(order);
+            }
+          })
+          .render(paypalRef);
+        })
+      }
+      loadButtonsAndLogicAboutPayment();
+    }
+  })
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {paid ? (
+        <div>
+          <h1>Parabéns você comprou o Plano!</h1>
+        </div>
+      ) : (
+        <>
+        <h1>{product.description} por R${product.price}</h1>
+        <div ref={v => (paypalRef = v)} />
+        </>
+      )}
     </div>
+    </>
   );
 }
 
